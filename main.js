6b6f17fa5e8f42a13567d379ad48ad7510a3f8ca
Spencer White <swhite215@live.com>
2017-02-17 16:20:44 -0500

    Way too late commit

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 
 $(document).ready(function() {
-  $.get("https://www.reddit.com/r/aww/.json").done(function(responseBody) {
-    var postsArray = responseBody.data.children;
-    for (var i = 0; i < 10; i++) {
+   $.get("https://www.reddit.com/r/awww/.json").done(function(responseBody) {
 
-      //postsArray[i].data.title = titles
-      console.log(postsArray[i].data.title);
+     var array = responseBody.data.children;
 
-      //postsArray[i].data.author = author
-      console.log(postsArray[i].data.author);
+    //  array[i].data.author gives us author
+    //  console.log(array[i].data.author);
+
+    //  array[i].data.title gives us title
+    //  console.log(array[i].data.title);
+
+    //for loop for moving through the array.
+     for (var i = 0; i < 10; i++) {
+     //array[i].data.preview.images[0].source.url gives us image url
+      var imgSource = array[i].data.preview.images[0].source.url;
+
+     //this code puts that title onto the body using append
+      var title = $("<h1>").text(array[i].data.title);
+      $("body").append(title);
+
+     //this code puts the authors name onto the body using append
+      var author = $("<h3>").text(array[i].data.author);
+      $("body").append(author);
+
+     //this code puts the image on the page
+      var image = $("<img>").attr("src", imgSource).attr("height", "100px").attr("width", "100px");
+      $("body").append(image);
+
     }
-    // console.log("DONE", responseBody.data.children);
-  }).fail(function() {
-    console.log("Hello");
-  }).always(function() {
-    console.log("Always");
-  })
+   });
 });
